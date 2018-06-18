cddc25b1c5b616878e27429808e281b7e057a030
Dave Richardson <daverichardson67@live.com>
2018-06-18 14:42:51 -0700

    Added Unit tests

---
+++ Tests/ContinuousWeb.Tests/HomeControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ContinuousWeb.Controllers;
+
+namespace ContinuousWeb.Tests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void AboutPageTest()
+        {
+            var homeController = new HomeController();
+            var result = homeController.About();
+        }
+    }
+}
